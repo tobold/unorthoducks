2253db7e788d2ec38ecd343778509d263aa22846
Toby Hamand <tobyhamand@gmail.com>
2017-09-13 11:49:29 +0100

    added AR

---
+++ Assets/spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+
+	// Use this for initialization
+	public void SpawnSphere () {
+		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.AddComponent<Rigidbody>();
+		sphere.transform.position = new Vector3(1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
