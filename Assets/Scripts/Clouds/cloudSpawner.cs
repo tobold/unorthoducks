c76cf559481bac6ad55c080a79043baaf9a48685
theartofnowt <michaelgordon.brown1@gmail.com>
2017-09-15 17:12:39 +0100

    further cloud works

--- Assets/Scripts/Clouds/cloudSpawner.cs
+++ Assets/Scripts/Clouds/cloudSpawner.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class cloudSpawner : MonoBehaviour {
 	
 	public GameObject puffCloud;
+	public GameObject puffCloud_0;
+	public GameObject puffCloud_1;
 
-	void Start () 
-		{
-		for (int i = 0; i < 1; i++)
-			Instantiate (puffCloud);
-		}
+	void Start ()
+	{
+		Instantiate (puffCloud);
+		Instantiate (puffCloud_0);
+		Instantiate (puffCloud_1);
+	}
 }
