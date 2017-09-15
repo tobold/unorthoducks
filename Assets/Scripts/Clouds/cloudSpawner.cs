d2ffa0addac8baa8b6274b9932b04f0436b322fa
theartofnowt <michaelgordon.brown1@gmail.com>
2017-09-15 16:58:05 +0100

    annoying clouds

---
+++ Assets/Scripts/Clouds/cloudSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudSpawner : MonoBehaviour {
+	
+	public GameObject puffCloud;
+
+	void Start () 
+		{
+		for (int i = 0; i < 1; i++)
+			Instantiate (puffCloud);
+		}
+}
