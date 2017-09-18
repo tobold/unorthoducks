51d078f11b9f85bc9b04a3806dbdc754f140d081
Alison Woodman <aliwoodman@gmail.com>
2017-09-18 18:20:20 +0100

    GameOver scene with final score

---
+++ Assets/Scripts/GameOver/GameOverText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unorthoducks
+{
+	public class GameOverText : MonoBehaviour {
+
+		public Text finalScore;
+
+		void Start () {
+			finalScore.text = "Gameover! You scored: " + ScoreManager.Score().ToString ();
+		}
+
+		void Update () {
+
+		}
+	}
+}
--- Assets/Scripts/Main/Game/GameState.cs
+++ Assets/Scripts/Main/Game/GameState.cs
@@ -6,9 +6,9 @@ namespace Unorthoducks
 {
   public class GameState : MonoBehaviour
   {
+
     public void Start()
     {
-      Debug.Log(GameManager.IsGameOver());
     }
 
     public void FixedUpdate()
@@ -16,11 +16,11 @@ namespace Unorthoducks
       Debug.Log(GameManager.DeadDucks());
       if(GameManager.IsGameOver()) {
         Time.timeScale = 0;
-        Debug.Log("you lose");
-        // SceneManager.LoadScene("GameOver");
+        SceneManager.LoadScene("GameOver");
       } else if(GameManager.LevelUp()) {
         Debug.Log("Level finished");
       }
     }
+
   }
 }
