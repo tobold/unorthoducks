9e1aead3335512f516f54f22a623da41d8137ec0
Toby Hamand <tobyhamand@gmail.com>
2017-09-21 17:05:24 +0100

    fixed soundmanager

--- Assets/Scripts/Main/Sounds/SoundManager.cs
+++ Assets/Scripts/Main/Sounds/SoundManager.cs
@@ -5,11 +5,12 @@ namespace Unorthoducks
 	[RequireComponent(typeof(AudioSource))]
 	public class SoundManager : MonoBehaviour
 	{
-    public AudioSource[] sounds;
+    public AudioSource[] biteSounds;
+		public AudioSource[] quackSounds;
 
     public void InitialiseQuacks (AudioSource[] soundArray)
     {
-      sounds = soundArray;
+      quackSounds = soundArray;
       float randomQuackTime = Random.Range(1f, 6f);
       Invoke("Quack", randomQuackTime);
     }
@@ -17,16 +18,15 @@ namespace Unorthoducks
     public void Quack ()
     {
       int randomSample = Random.Range(0, 3);
-      sounds[randomSample].Play();
+      quackSounds[randomSample].Play();
       float randomQuackTime = Random.Range(5f, 10f);
       Invoke("Quack", randomQuackTime);
     }
 
     public void BiteSound (AudioSource[] soundArray)
     {
-      sounds = soundArray;
-      sounds[0].Play();
+      biteSounds = soundArray;
+      biteSounds[0].Play();
     }
-
   }
 }
