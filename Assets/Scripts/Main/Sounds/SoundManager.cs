374e63b3dc8b01c5029bf7f581fe8be86dd24212
Alison Woodman <aliwoodman@gmail.com>
2017-09-21 12:56:16 +0100

    Extract sound logic into SoundManger class (#49)
    * Quacking delegated to SoundManager
    
    * Bite sounds delegated to sound manager

---
+++ Assets/Scripts/Main/Sounds/SoundManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Unorthoducks
+{
+	[RequireComponent(typeof(AudioSource))]
+	public class SoundManager : MonoBehaviour
+	{
+    public AudioSource[] sounds;
+
+    public void InitialiseQuacks (AudioSource[] soundArray)
+    {
+      sounds = soundArray;
+      float randomQuackTime = Random.Range(1f, 6f);
+      Invoke("Quack", randomQuackTime);
+    }
+
+    public void Quack ()
+    {
+      int randomSample = Random.Range(0, 3);
+      sounds[randomSample].Play();
+      float randomQuackTime = Random.Range(5f, 10f);
+      Invoke("Quack", randomQuackTime);
+    }
+
+    public void BiteSound (AudioSource[] soundArray)
+    {
+      sounds = soundArray;
+      sounds[0].Play();
+    }
+
+  }
+}
